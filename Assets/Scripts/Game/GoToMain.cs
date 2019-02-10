539c6a6b4fa6681ff7382eae820e15329b340a3a
탁동혁 <tdh8316@naver.com>
2019-02-10 17:49:41 +0900

    게임 재시작시 요소가 충분히 초기화되지 않음

--- Assets/Scripts/Game/GoToMain.cs
+++ Assets/Scripts/Game/GoToMain.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GoToMain : MonoBehaviour
 {
     public void LoadMainScene()
     {
+        BlockScript.script = new List<int>();
         SceneManager.LoadScene(0);
     }
 }
