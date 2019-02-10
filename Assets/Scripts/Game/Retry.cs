118989635cffce94c04117f4e2d93e2f180fa035
탁동혁 <tdh8316@naver.com>
2019-02-10 17:46:20 +0900

    재시작 시 스크립트가 초기화되지 않던 문제 해결

--- Assets/Scripts/Game/Retry.cs
+++ Assets/Scripts/Game/Retry.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Retry : MonoBehaviour
 {
     public void ReloadScene()
     {
+        BlockScript.script = new List<int>();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
