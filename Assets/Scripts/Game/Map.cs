559568edcf07926391ec3dd4178a86cbe082fe81
탁동혁 <tdh8316@naver.com>
2019-02-10 13:46:46 +0900

    맵을 벗어난 경우 게임오버

---
+++ Assets/Scripts/Game/Map.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Map : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
