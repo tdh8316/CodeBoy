b96e206180fc345a03a3d1cec8f94454b7812cb7
탁동혁 <tdh8316@naver.com>
2019-02-10 17:34:37 +0900

    게임 시 불필요한 요소 제거

--- Assets/Scripts/Game/Map.cs
+++ Assets/Scripts/Game/Map.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        // Init
         BlockScript.script = new List<int>();
+        GameObject.Find("scriptText").GetComponent<Text>().text = "";
     }
 
     // Update is called once per frame
