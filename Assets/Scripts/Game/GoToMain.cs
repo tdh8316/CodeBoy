54f7864869e3d8c7c72eff3333e7283782448396
탁동혁 <tdh8316@naver.com>
2019-02-10 13:39:48 +0900

    메인으로 돌아가기

---
+++ Assets/Scripts/Game/GoToMain.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMain : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
